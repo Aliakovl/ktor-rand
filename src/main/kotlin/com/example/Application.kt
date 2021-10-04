c6ed999908a318a92faffb06f30bfc031003a966
aleksei <aleksei.iakowlew@gmail.com>
2021-10-04 12:35:41 +0300

    add project

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+
+fun runServer(port: Int = 8080){
+    embeddedServer(Netty, port = port, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        runServer()
+    }
+    else if (args[0] == "--port"){
+        val a: Int? = try {
+            val p = args[1].toInt()
+            if (p in 1..9999) p else throw RuntimeException("Incorrect port")
+        } catch (e: NumberFormatException) {
+            throw RuntimeException("Incorrect port")
+        }
+        a?.let { runServer(it) }
+    }
+    else {
+        throw RuntimeException("Invalid option ${args[0]}")
+    }
+}
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,51 @@
+package com.example.plugins
+
+import kotlin.random.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun Application.configureRouting() {
+
+    val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    fun randomString(x: Int) = (1..x)
+        .map { _ -> Random.nextInt(0, charPool.size) }
+        .map(charPool::get)
+        .joinToString("")
+
+    fun getFormattedDate(): String {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss.SSS")
+        return LocalDateTime.now().format(formatter)
+    }
+
+    routing {
+        get("/*") {
+            call.request.queryParameters["length"]?.let { length ->
+                try {
+                    val randStr = randomString(length.toInt())
+                    call.respondText(randStr, ContentType.Text.Plain, HttpStatusCode.OK)
+                    File("log").appendText("${getFormattedDate()} - $randStr\n")
+                } catch (e: NumberFormatException){
+                    call.respond(HttpStatusCode.BadRequest, "to large length")
+                }
+            }
+            call.request.queryParameters["begin"]?.let { begin ->
+                call.request.queryParameters["end"]?.let { end ->
+                    try {
+                        val randInt = Random.nextInt(begin.toInt(), end.toInt()+1).toString()
+                        File("log").appendText("${getFormattedDate()} - $randInt\n")
+                        call.respondText(randInt, ContentType.Text.Plain, HttpStatusCode.OK)
+                    } catch (e: NumberFormatException){
+                        call.respond(HttpStatusCode.BadRequest, "to large begin or end")
+                    }
+                }
+            }
+            call.request.let { _ -> call.respond(HttpStatusCode.NotFound, "no page found!") }
+        }
+    }
+}
